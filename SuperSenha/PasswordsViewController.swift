e90639c2993ed367bf5c586651348f9911b8f0e1
Luciano Borges <lucianosantosborges@gmail.com>
2019-08-18 14:13:13 +0100

    Geração das senhas e ajustes finais

--- SuperSenha/PasswordsViewController.swift
+++ SuperSenha/PasswordsViewController.swift
@@ -21,10 +21,26 @@ class PasswordsViewController: UIViewController {
     var useCapitalLetters: Bool!
     var useSpecialCharacters: Bool!
     
+    var passwordGenerator: PasswordGenerator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Total de senhas: \(numberOfPasswords)"
+        passwordGenerator = PasswordGenerator(numberOfCharacters: numberOfCharacters, useLetters: useLetters, useNumbers: useNumbers, useCapitalLetters: useCapitalLetters, useSpecialCharacters: useSpecialCharacters)
+            
+        generatePasswords();
+    }
+    
+    func generatePasswords() {
+        tvPasswords.scrollRangeToVisible(NSRange(location: 0, length: 0))
+        tvPasswords.text = ""
+        let passwords = passwordGenerator.generate(total: numberOfPasswords)
+        for password in passwords {
+            tvPasswords.text.append(password + "\n\n")
+        }
     }
     
     @IBAction func generate(_ sender: UIButton) {
+        generatePasswords();
     }
 }
